88b8908e0011b49318a33df1a717110a82fb9567
David <davruiz07@gmail.com>
2022-03-10 18:11:12 -0500

    initial commit

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,12 +1,1 @@
-import React from 'react'
-
-export default class Home extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <h3>Welcome?</h3>
-      </div >
-    )
-  }
-}
+//start here
